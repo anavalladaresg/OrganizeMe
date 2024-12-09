bebb7fbca4b19919c0439caec661002ba01e0140
Ana <avalladaresgonzalez@danielcastelao.org>
2024-12-09 19:30:31 +0100

    Cambios en DashboardFragment

--- app/src/main/java/com/example/organizeme/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/organizeme/ui/dashboard/DashboardFragment.kt
@@ -1,11 +1,13 @@
 package com.example.organizeme.ui.dashboard
 
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-import com.example.organizeme.R
 
 /**
  * Un [Fragment] simple que representa la pantalla del tablero.
@@ -18,13 +20,22 @@ class DashboardFragment : Fragment() {
      * @param inflater El objeto LayoutInflater que se puede usar para inflar cualquier vista en el fragmento.
      * @param container Si no es nulo, esta es la vista principal a la que debe adjuntarse la interfaz de usuario del fragmento.
      * @param savedInstanceState Si no es nulo, este fragmento se está reconstruyendo a partir de un estado guardado anterior proporcionado aquí.
-     * @return Devuelve la Vista para la interfaz de usuario del fragmento, o nulo.
+     * @return Devuelve la Vista para la interfaz de usuario del fragmento.
      */
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Infla el diseño para este fragmento
-        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return LinearLayout(requireContext()).apply {
+            layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT
+            )
+            orientation = LinearLayout.VERTICAL
+            gravity = Gravity.CENTER
+
+            addView(TextView(requireContext()).apply {
+                text = "Pantalla del Dashboard"
+                textSize = 18f
+                gravity = Gravity.CENTER
+            })
+        }
     }
 }
